264f88ed11382ba6bd2008810f62dadbe7763053
unknown <j.guymont@gmail.com>
2018-11-30 15:48:06 -0500

    adaboost classifier

--- models.py
+++ models.py
@@ -13,8 +13,8 @@ import argparse
 import logging
 import numpy as np
 import os
-from sklearn.ensemble import AdaBoostRegressor
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 LOGGER = logging.getLogger(os.path.basename(__file__))
 
@@ -27,147 +27,33 @@ SETTINGS = {
 # controls how chatty RandomizedCV is
 VERB_LEVEL = 0
 
-
-def k_nn():
-    LOGGER.debug('building K-NN model')
+def decision_tree(data, adaboost=True):
+    LOGGER.debug('building decision tree model')
     # hyperparameters to search for randomized cross validation
     settings = {
-        'dim__n_components': stats.randint(10, 1000),
-        'clf__tol': stats.uniform(10e-5, 10e-1),
-        'clf__C': stats.uniform(10e-3, 1),
-        'clf__n_neighbors': stats.randint(1,50)
+        'dim__n_components': stats.randint(1, 11),
+        #'clf__max_depth': stats.randint(1, 12),
+        #'clf__min_samples_split': stats.randint(2, 5),
+        #'clf__min_samples_leaf': stats.randint(1, 5),
+        #'clf__max_features': stats.randint(1, 12)
+        #'clf__n_estimators':[25,30,35,40,45,50],
+        #'clf__learning_rate':[0.01,0.02,0.04,0.08,0.16,0.32,0.64,1.28,2.56]
+        
     }
 
-    # model we will train in our pipeline
-    clf = KNeighborsClassifier()
+    clf = AdaBoostClassifier(DecisionTreeClassifier())
 
-    # pipeline runs preprocessing and model during every CV loop
-    pipe = Pipeline([
+    pipeline = Pipeline([
         ('pre', StandardScaler()),
         ('dim', PCA(svd_solver='randomized')),
         ('clf', clf),
     ])
 
     # this will learn our best parameters for the final model
-    model = RandomizedSearchCV(pipe, settings, n_jobs=-1, verbose=VERB_LEVEL,
+    model = RandomizedSearchCV(pipeline, settings, n_jobs=-1, verbose=VERB_LEVEL,
         n_iter=SETTINGS['n_cv'], cv=SETTINGS['n_inner'], scoring='accuracy'
     )
 
     return model
-
-
-
-def SVM_nonlinear(data):
-    """soft SVM with kernel"""
-    LOGGER.debug('building SVM model')
-    # hyperparameters to search for randomized cross validation
-    settings = {
-        'dim__n_components': stats.randint(10, 1000),
-        'clf__tol': stats.uniform(10e-5, 10e-1),
-        'clf__C': stats.uniform(10e-3, 1)
-    }
-
-    # model we will train in our pipeline
-    clf = SVC(gamma=0.001, max_iter=100)
-
-    # pipeline runs preprocessing and model during every CV loop
-    pipe = Pipeline([
-        ('pre', StandardScaler()),
-        ('dim', PCA(svd_solver='randomized')),
-        ('clf', clf),
-    ])
-
-    # this will learn our best parameters for the final model
-    model = RandomizedSearchCV(pipe, settings, n_jobs=-1, verbose=VERB_LEVEL,
-        n_iter=SETTINGS['n_cv'], cv=SETTINGS['n_inner'], scoring='accuracy'
-    )
-
-    return(model)
-
-
-
-def SVM(data):
-    """ baseline: linear classifier (without kernel)"""
-    LOGGER.debug('building SVM model')
-    # hyperparameters to search for randomized cross validation
-    settings = {
-        'dim__n_components': stats.randint(10,1000),
-        'clf__tol': stats.uniform(10e-5, 10e-1),
-        'clf__C': stats.uniform(10e-3, 1)
-    }
-
-    # model we will train in our pipeline
-    clf = SVC(kernel='linear', max_iter=100)
-
-    # pipeline runs preprocessing and model during every CV loop
-    pipe = Pipeline([
-        ('pre', StandardScaler()),
-        ('dim', PCA(svd_solver='randomized')),
-        ('clf', clf),
-    ])
-
-    # this will learn our best parameters for the final model
-    model = RandomizedSearchCV(pipe, settings, n_jobs=-1, verbose=VERB_LEVEL,
-        n_iter=SETTINGS['n_cv'], cv=SETTINGS['n_inner'], scoring='accuracy'
-    )
-
-    return(model)
-
-
-def logistic_regression(data):
-    """baseline: linear classifier"""
-    LOGGER.debug('building logistic regression model')
-    # hyperparameters to search for randomized cross validation
-    settings = {
-        'dim__n_components': stats.randint(10, 400),
-        'clf__tol': stats.uniform(10e-5, 10e-1),
-        'clf__C': stats.uniform(10e-3, 10),
-        'clf__penalty': ['l1', 'l2']
-    }
-
-    # model we will train in our pipeline
-    clf = LogisticRegression(solver='saga', multi_class='ovr', max_iter=100)
-
-    # pipeline runs preprocessing and model during every CV loop
-    pipe = Pipeline([
-        ('pre', StandardScaler()),
-        ('dim', PCA(svd_solver='randomized')),
-        ('clf', clf),
-    ])
-
-    # this will learn our best parameters for the final model
-    model = RandomizedSearchCV(pipe, settings, n_jobs=-1, verbose=VERB_LEVEL,
-        n_iter=SETTINGS['n_cv'], cv=SETTINGS['n_inner'], scoring='accuracy'
-    )
-
-    return(model)
-
-def Decision_tress(data, adaboost=True):
-    LOGGER.debug('building decision tree model')
-    # hyperparameters to search for randomized cross validation
-    settings = {
-        'clf__max_depth': stats.randint(1,12),
-        'clf__min_samples_split: stats.randint(1,5),
-        'clf__min_samples_leaf: stats.randint(1,5),
-        'clf__max_features: stats.randint(1,12)
-    }
-    #Not used: criterion, splitter, random_stat, max_leaf_nodes, min_impurity_decrease, min_impurity_split, class_weight
-
-    # model we will train in our pipeline
-    clf = DecisionTreeRegressor()
-
-    # pipeline runs preprocessing and model during every CV loop
-    pipe = Pipeline([
-        ('pre', StandardScaler()),
-        ('dim', PCA(svd_solver='randomized')),
-        ('clf', clf),
-    ])
-
-    # this will learn our best parameters for the final model
-    model = RandomizedSearchCV(pipe, settings, n_jobs=-1, verbose=VERB_LEVEL,
-        n_iter=SETTINGS['n_cv'], cv=SETTINGS['n_inner'], scoring='accuracy'
-    )
-
-    return(model)
 
 
