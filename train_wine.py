20eaa3b00bf4da4433a7b2e1eceaf482d5f606f4
unknown <j.guymont@gmail.com>
2018-11-30 15:08:41 -0500

    add arparser for model

--- train_wine.py
+++ train_wine.py
@@ -20,44 +20,30 @@ LOGGER = logging.getLogger()
 
 category_map = {}
 
-def main(test_mode=False):
+def main(args):
 
-    log_fname = "logs/{}.log".foramt(__name__)
+    log_fname = "logs/{}.log".format(__name__)
     if os.path.isfile(log_fname):
         os.remove(log_fname)
     log_hdl = logging.FileHandler(log_fname)
     log_hdl.setFormatter(logging.Formatter('%(message)s'))
     LOGGER.addHandler(log_hdl)
 
-    data = utils.load_wine(test_mode=test_mode, cropping=False)
-
-    # way to map between string labels and int labels
-    y_map = utils.get_y_map(data)
-    data['y']['train'] = utils.convert_y(data['y']['train'], y_map)
-    data['y']['valid'] = utils.convert_y(data['y']['valid'], y_map)
+    data = utils.load_wine(args.test)
 
     # run experiments
-    y_test = exp.resnet(data)
-    y_test = utils.convert_y(y_test, y_map)
-    utils.write_results('results/wine.csv', y_test)
-
-    #data = utils.load_data(test_mode=test_mode)
-    #lr_pred, lr_model = exp.lr_baseline(data)
-    #lr_y_test = utils.convert_y(lr_pred['test'], y_map)
-    #utils.write_results('results/lr_baseline.csv', lr_y_test)
-
-    #svm_pred, svm_model = exp.svm_baseline(data)
-    #svm_y_test = utils.convert_y(svm_pred['test'], y_map)
-    #utils.write_results('results/svm_baseline.csv', svm_y_test)
+    # TODO: add other models
+    if args.model == 'decision_tree':
+        results, best_model = exp.decision_tree(data)
+    utils.write_results('results/wine.csv', results)
 
 
 if __name__ == "__main__":
 
     argparser = argparse.ArgumentParser()
-    argparser.add_argument("-v", "--verbose", action="store_true",
-        help="increase output verbosity")
-    argparser.add_argument("-t", "--test", action="store_true",
-        help="training set size=500")
+    argparser.add_argument("-v", "--verbose", action="store_true", help="increase output verbosity")
+    argparser.add_argument("-t", "--test", action="store_true", help="training set size=500")
+    argparser.add_argument("-m", "--model", help="which model")
     args = argparser.parse_args()
 
     if args.verbose:
@@ -65,6 +51,6 @@ if __name__ == "__main__":
     else:
         LOGGER.setLevel(logging.INFO)
 
-    main(test_mode=args.test)
+    main(args)
 
 
