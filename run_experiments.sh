7cf84c35134fca2fdb988fef286246a2a75d6c9a
Joseph Viviano <joseph@viviano.ca>
2018-12-04 09:06:07 -0500

    added experiment file

---
+++ run_experiments.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+./train.py -v -m tree -d wine
+./train.py -v -m tree -d covtype
+./train.py -v -m tree -d covtype_balanced
+./train.py -v -m svm -d wine
+./train.py -v -m svm -d covtype
+./train.py -v -m svm -d covtype_balanced
+./train.py -v -m mlp -d wine
+./train.py -v -m mlp -d covtype
+./train.py -v -m mlp -d covtype_balanced
+
